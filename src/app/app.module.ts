b15c12fecb82157c7a7ba7a8fa5571f485a5325a
ste@mcb.dk <stefan@nolleren.org>
2017-11-01 20:49:28 +0100

    test of web.config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { BlueComponent } from './blue/blue.component';
+import { RedComponent } from './red/red.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: 'blue', component: BlueComponent },
+  { path: 'red', component: RedComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlueComponent,
+    RedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

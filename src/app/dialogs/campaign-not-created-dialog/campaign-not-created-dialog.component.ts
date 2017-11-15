13f750f8c4b7d78f13332ee3de5ee93615556d17
ste@mcb.dk <stefan@nolleren.org>
2017-11-15 19:54:23 +0100

    now with observables

---
+++ src/app/dialogs/campaign-not-created-dialog/campaign-not-created-dialog.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'campaign-not-created-dialog',
+    templateUrl: 'campaign-not-created-dialog.component.html',
+  })
+  export class CampaignNotCreatedDialogComponent {}
---
+++ src/app/dialogs/campaign-selected-dialog/campaign-selected-dialog.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-selected-dialog',
+  templateUrl: './campaign-selected-dialog.component.html'
+})
+export class CampaignSelectedDialogComponent{}

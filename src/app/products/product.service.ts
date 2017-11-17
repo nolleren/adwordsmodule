507e9a4f1d4956212580f4c1efdbf32ea422bf44
ste@mcb.dk <stefan@nolleren.org>
2017-11-17 15:25:49 +0100

    problems with adding a listItem

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,12 +1,13 @@
 import { Subject } from 'rxjs/Subject';
 import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Product } from '../../models/product';
 
 @Injectable()
 export class ProductService {
-  
-  constructor(private http: Http) { }
+  httpString: string;
+
+  constructor(private http: Http) {}
 
   // Observable string source
   DataProductListSource = new Subject<Product[]>();
@@ -19,9 +20,7 @@ export class ProductService {
     this.DataProductListSource.next(products);
   }
   
-
   getProducts(){
-    //return this.http.get("http://adwordsmoduleapi.azurewebsites.net/api/products").map(res => res.json());
-    return this.http.get("http://localhost:52185/api/products").map(res => res.json());
+    return this.http.get("http://adwordsmoduleapi.azurewebsites.net/api/products").map(res => res.json());
   }
 }
